e82844fc7693b75c038aca4f31f1715c2a504489
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2021-02-26 10:58:27 +0100

    remove excess features

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import "./App.scss";
 
 import Landing from "./pages/Landing/Landing";
 
 const App = () => {
   return (
-   <div className='App'>
-      <Router>
-      
-      <Switch>
-        <Route path="/" exact component={Landing} />
-
-      </Switch>
-    </Router>
-   </div>
+    <div className="App">
+      <Landing />
+    </div>
   );
 };
 
